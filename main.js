a252e8caa2986292beefbfdad1022d5b1ed10257
Barbara Stadnik <barbara1stadnik@gmail.com>
2020-05-19 08:53:57 +0200

    get values from objects nested in array nested

---
+++ main.js
@@ -0,0 +1,21 @@
+(function() {
+	'use strict';
+
+	const listJaneFriends = document.querySelector('.jane-friends .friends-list'),
+		displayJaneFriendsButton = document.querySelector('.jane-friends button'),
+		people = [
+		    { name: 'John Doe', age: 50 },
+		    { name: 'Jane Doe', age: 20, friends: [{ name: 'Annie' }, { name: 'Betty' }, { name: 'Cindy' }] }
+		];
+
+	function getAndDisplayFriends() {
+		const [, {friends: [{name: friend1}, {name: friend2}, {name: friend3}]}] = people,
+			janeFriends = [friend1, friend2, friend3];
+
+			let result = listJaneFriends.innerHTML = janeFriends.map((friend, idx) => {
+				return `<li>${friend}</li>`;
+			}).join('');
+	}
+
+	displayJaneFriendsButton.addEventListener('click', getAndDisplayFriends);
+})();
